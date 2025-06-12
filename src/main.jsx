8d73bf9defdc9d125e5066e7b0482d8e551d05a6
Suela Carkaxhija <suela.carkaxhija@gmail.com>
2025-06-12 17:16:46 +0200

    fixed website root issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/personal-website-app'>
+    <HashRouter basename='/personal-website-app'>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
